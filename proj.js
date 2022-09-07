73b2e4002a970ad8de7c3ea20431b0b9a0f9be48
jeanfrontdev <jeanacer215@gmail.com>
2022-09-07 10:26:02 +0800

    first commit

---
+++ proj.js
@@ -0,0 +1,21 @@
+//Nav bar effects on scroll
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+  
+  //Services sec - Modal
+  
+  //Portfolio sec - Modal
+  
+  //My clients - Swiper
+  
+  //Scroll to top btn
+  
+  //Nav menu items active on page scroll
+  
+  //Responsive nav menu toggle
+  
+  //scroll reveal animations
+  
+  //Common reveal options to create reveal animations
